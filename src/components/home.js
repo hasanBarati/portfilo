8efd3155a713ab3201c01691ff363f4d34589f80
Hasan <hasanbaratiasp@gmail.com>
2022-07-18 15:57:08 +0430

    create portfilo with tailwind

--- src/components/home.js
+++ src/components/home.js
@@ -9,7 +9,7 @@ export const Home=()=>{
                         i am a front-end developer
                     </h2>
                     <p className="text-gray-500 max-w-md py-4">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                        Seasoned and independent Front End Developer with 3 years of experience in blending the art of design with skill of programming to deliver an immersive and engaging user experience through efficient website development, proactive feature optimization, and relentless debugging.
                     </p>
 
 
